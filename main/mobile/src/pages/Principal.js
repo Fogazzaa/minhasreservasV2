a23cf52e1fb7e53a04af788bcbd86a8355e01592
fogazza <vinicius.cintra2801@gmail.com>
2025-03-02 17:28:02 -0300

    02-03-25 / adição e atualização da pagina Principal, adicionando as tabelas

--- main/mobile/src/pages/Principal.js
+++ main/mobile/src/pages/Principal.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Image,
+  FlatList,
   ImageBackground,
   StyleSheet,
-  TouchableOpacity,
-  View
+  Text,
+  View,
 } from "react-native";
+import api from "../services/axios";
 
-export default function Principal({ navigation }) {
+function Principal({ navigation }) {
+  const [salas, setSalas] = useState([]);
+
+  async function getSalas() {
+    await api.getSalas().then(
+      (response) => {
+        setSalas(response.data.salas);
+      },
+      (error) => {
+        console.log("Erro", error);
+      }
+    );
+  }
+
+  useEffect(() => {
+    getSalas();
+  }, []);
+
+  const listSalas = ({ item }) => (
+    <View style={styles.row}>
+      <Text style={[styles.tableCell, styles.nome]}>{item.nome}</Text>
+      <Text style={[styles.tableCell, styles.descricao]}>{item.descricao}</Text>
+      <Text style={[styles.tableCell, styles.bloco]}>{item.bloco}</Text>
+      <Text style={[styles.tableCell, styles.tipo]}>{item.tipo}</Text>
+      <Text style={[styles.tableCell, styles.capacidade]}>
+        {item.capacidade}
+      </Text>
+    </View>
+  );
+
   return (
     <ImageBackground
       source={require("../../img/fundo.png")}
@@ -15,58 +45,76 @@ export default function Principal({ navigation }) {
       resizeMode="cover"
     >
       <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.buttonToHome}
-          onPress={() => navigation.navigate("Login")}
-        >
-          <Image
-            source={require("../../img/botaohome.png")}
-            style={styles.imageButtonToHome}
-          />
-        </TouchableOpacity>
+        <View style={styles.tableHeader}>
+          <Text style={styles.tableHeaderCell}>Nome</Text>
+          <Text style={styles.tableHeaderCell}>Descrição</Text>
+          <Text style={styles.tableHeaderCell}>Bloco</Text>
+          <Text style={styles.tableHeaderCell}>Tipo</Text>
+          <Text style={styles.tableHeaderCell}>Capacidade</Text>
+        </View>
+        <FlatList
+          data={salas}
+          renderItem={listSalas}
+          keyExtractor={(sala) => sala.id_sala.toString()}
+        />
       </View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  container: { padding: 20 },
+  tableHeader: {
+    flexDirection: "row",
+    backgroundColor: "gray",
+    padding: 10,
+    borderWidth: 2,
+    borderColor: "white",
+  },
+  tableHeaderCell: {
     flex: 1,
-    justifyContent: "center",
+    marginLeft:10,
     alignItems: "center",
-  },
-  container: {
-    flex: 1,
-    width: "100%",
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
-    padding: 20,
-    backgroundColor: "transparent",
-  },
-  buttonToHome: {
-    position: "absolute",
-    top: 20,
-    left:20,
-    width: 50,
-    height: 50,
-    backgroundColor: "white",
-    borderRadius: 100,
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  imageButtonToHome: {
-    width: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    resizeMode: "contain",
-  },
-  title: {
-    fontSize: 25,
     fontWeight: "bold",
-    marginBottom: 20,
-  },
-  text: {
     color: "white",
   },
+  row: {
+    justifyContent: "flex-start",
+    alignItems: "center",
+    justifyContent: "end",
+    flexDirection: "row",
+    paddingVertical: 10,
+    paddingHorinzontal: 10,
+    borderWidth: 1.5,
+    borderColor: "white",
+    backgroundColor: "#949494",
+  },
+  tableCell: {
+    marginLeft:2,
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    flex: 1,
+    color: "white",
+    textAlign: "center",
+  },
+  nome: {
+    flex: 1.3,
+    fontWeight: "bold",
+  },
+  descricao: {
+    flex: 2,
+  },
+  bloco: {
+    flex: 1,
+  },
+  tipo: {
+    flex: 1.2,
+    fontStyle: "italic",
+  },
+  capacidade: {
+    flex: 1,
+  },
 });
+
+export default Principal;
