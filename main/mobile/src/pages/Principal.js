bffbbd4e11bf08cfa14920ffa97023268182741c
fogazza <vinicius.cintra2801@gmail.com>
2025-03-03 14:51:03 -0300

    03-03-25 / adição e atualização das páginas Mobile: Principal/Login e Cadastro

--- main/mobile/src/pages/Principal.js
+++ main/mobile/src/pages/Principal.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {
   FlatList,
+  Image,
   ImageBackground,
   StyleSheet,
+  TouchableOpacity,
   Text,
   View,
 } from "react-native";
@@ -44,13 +46,31 @@ function Principal({ navigation }) {
       style={styles.background}
       resizeMode="cover"
     >
-      <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("../../img/logo.png")} style={styles.logo} />
+        <TouchableOpacity
+          style={styles.buttonToHome}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Image
+            source={require("../../img/iconelogout.png")}
+            style={styles.imageButtonToHome}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.body}>
         <View style={styles.tableHeader}>
-          <Text style={styles.tableHeaderCell}>Nome</Text>
-          <Text style={styles.tableHeaderCell}>Descrição</Text>
-          <Text style={styles.tableHeaderCell}>Bloco</Text>
-          <Text style={styles.tableHeaderCell}>Tipo</Text>
-          <Text style={styles.tableHeaderCell}>Capacidade</Text>
+          <Text style={[styles.tableHeaderCell, styles.headerNome]}>Nome</Text>
+          <Text style={[styles.tableHeaderCell, styles.headerDescricao]}>
+            Descrição
+          </Text>
+          <Text style={[styles.tableHeaderCell, styles.headerBloco]}>
+            Bloco
+          </Text>
+          <Text style={[styles.tableHeaderCell, styles.headerTipo]}>Tipo</Text>
+          <Text style={[styles.tableHeaderCell, styles.headerCapacidade]}>
+            Cap.
+          </Text>
         </View>
         <FlatList
           data={salas}
@@ -58,63 +78,152 @@ function Principal({ navigation }) {
           keyExtractor={(sala) => sala.id_sala.toString()}
         />
       </View>
+      <View style={styles.footer}>
+        <Text style={styles.textDesenvolvido}>
+          &copy; Desenvolvido por: Vinicius Fogaça, Maria Júlia e Maria Fernanda
+        </Text>
+      </View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { padding: 20 },
+  background: { height: "100vh" },
+  header: {
+    backgroundColor: "rgba(177, 16, 16, 1)",
+    height: 60,
+    borderBottomColor: "white",
+    borderBottomWidth: 3,
+    flexDirection: "row",
+  },
+  logo: {
+    justifyContent: "center",
+    alignItems: "center",
+    resizeMode: "contain",
+    width: 120,
+    height: 34,
+    marginTop: 12,
+    marginLeft: 10,
+    borderRadius: 8,
+    borderColor: "white",
+    borderWidth: 3,
+  },
+  buttonToHome: {
+    justifyContent: "center",
+    alignItems: "center",
+    resizeMode: "contain",
+    marginLeft: 230,
+  },
+  imageButtonToHome: {
+    justifyContent: "center",
+    alignItems: "center",
+    resizeMode: "contain",
+    width: 37,
+    height: 37,
+  },
+  body: {
+    padding: 8,
+    paddingTop: 10,
+    paddingBottom: 180,
+  },
   tableHeader: {
     flexDirection: "row",
     backgroundColor: "gray",
-    padding: 10,
+    paddingVertical: 10,
     borderWidth: 2,
     borderColor: "white",
   },
   tableHeaderCell: {
-    flex: 1,
-    marginLeft:10,
+    textAlign: "center",
     alignItems: "center",
-    justifyContent: "center",
     fontWeight: "bold",
     color: "white",
+    paddingHorizontal: 5,
+    paddingVertical: 5,
   },
+  headerNome: { width: 40, marginLeft: 30 },
+  headerDescricao: { width: 100, marginLeft: 40 },
+  headerBloco: { width: 70 },
+  headerTipo: { width: 80, marginLeft: -12 },
+  headerCapacidade: { width: 80, marginLeft: -20 },
+
   row: {
-    justifyContent: "flex-start",
-    alignItems: "center",
-    justifyContent: "end",
     flexDirection: "row",
+    alignItems: "flex-start",
     paddingVertical: 10,
-    paddingHorinzontal: 10,
     borderWidth: 1.5,
     borderColor: "white",
     backgroundColor: "#949494",
   },
-  tableCell: {
-    marginLeft:2,
-    paddingHorizontal: 2,
-    paddingVertical: 5,
-    flex: 1,
-    color: "white",
-    textAlign: "center",
-  },
   nome: {
-    flex: 1.3,
+    width: 100,
+    flexWrap: "wrap",
+    textAlign: "center",
+    justifyContent: "center",
+    color: "white",
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    borderRightWidth: 0.5,
+    borderRightColor: "white",
     fontWeight: "bold",
   },
   descricao: {
-    flex: 2,
+    width: 120,
+    flexWrap: "wrap",
+    textAlign: "center",
+    color: "white",
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    borderLeftWidth: 0.5,
+    borderLeftColor: "white",
+    borderRightWidth: 0.5,
+    borderRightColor: "white",
   },
   bloco: {
-    flex: 1,
+    width: 50,
+    flexWrap: "wrap",
+    textAlign: "center",
+    color: "white",
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    borderLeftWidth: 0.5,
+    borderLeftColor: "white",
+    borderRightWidth: 0.5,
+    borderRightColor: "white",
   },
   tipo: {
-    flex: 1.2,
+    width: 80,
+    flexWrap: "wrap",
+    textAlign: "center",
+    color: "white",
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    borderLeftWidth: 0.5,
+    borderLeftColor: "white",
+    borderRightWidth: 0.5,
+    borderRightColor: "white",
     fontStyle: "italic",
   },
   capacidade: {
-    flex: 1,
+    width: 40,
+    flexWrap: "wrap",
+    textAlign: "center",
+    color: "white",
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    borderLeftWidth: 0.5,
+    borderLeftColor: "white",
   },
+  footer: {
+    backgroundColor: "rgb(166, 13, 13)",
+    height: 50,
+    borderTopColor: "white",
+    borderTopWidth: 3,
+    marginTop: -158,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textDesenvolvido: { color: "white", fontWeight: "bold" },
 });
 
 export default Principal;
